fbca9d969d5469167ec4e919447b77e97920097f
Alberto Chen <ac11599@nyu.edu>
2026-06-22 18:21:09 +0000

    added dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+
+# --- dataset link --- 
+# https://www.kaggle.com/datasets/zafarali27/house-price-prediction-dataset
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
