0b75b23a353da7e6ab8cde4b427b66ade6cbbe02
Alberto Chen <ac11599@nyu.edu>
2026-06-22 18:51:59 +0000

    setup layout for python file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,62 @@
+# --- imports ---
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
 
 # --- dataset link --- 
 # https://www.kaggle.com/datasets/zafarali27/house-price-prediction-dataset
 
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# --- streamlit initial setup ---
+st.set_page_config(
+    page_title="Australian Housing Dashboard 🏡",
+    layout="centered",
+    page_icon="🏡",
 )
+
+st.sidebar.title("Australian Housing Dashboard 🏡")
+page = st.sidebar.selectbox(
+    "Select Page",
+    [
+        "Introduction 📘",
+        "Data Visualization 📊",
+        "Prediction 🔮",
+        "Explainability 🔍",
+        "Hyperparameter Tuning 📈",
+        "Conclusion 📖",
+    ],
+)
+
+st.image("house.jpg", width=500)
+
+df = pd.read_csv("house_pricing_dataset.csv")
+
+# --- introduction ---
+if page == "Introduction 📘":
+    st.header("01 Introduction 📘")
+
+# --- visualization ---
+elif page == "Data Visualization 📊":
+    st.header("02 Data Visualization 📊")
+    
+
+# --- prediction ---
+elif page == "Prediction 🔮":
+    st.header("03 Prediction 🔮")
+
+
+# --- explainability page ---
+elif page == "Explainability 🔍":
+    st.header("04 Explainability 🔍")
+    
+
+# --- MLflow Runs Page ---
+elif page == "Hyperparameter Tuning 📈":
+    st.header("05 Hyperparameter Tuning 📈")
+    
+
+# --- conclusion ---
+elif page == "Conclusion 📖":
+    st.header("06 Conclusion 📖")
+
